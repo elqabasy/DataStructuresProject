6e207ecab5dc11f5e94e86280c4fce0dc33fe7f5
elqabasy <mahros.elqabasy@hotmail.com>
2024-12-29 00:39:21 +0200

    Init

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_DataStructuresProject2_FILE F:/Datastructures/projects/Mohamed AL-Sayed/DataStructuresProject2/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/DataStructuresProject2.exe)
+set(__QT_DEPLOY_TARGET_DataStructuresProject2_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_DataStructuresProject2_RUNTIME_DLLS C:/Qt-Latest/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt-Latest/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt-Latest/6.8.0/mingw_64/bin/Qt6Core.dll)
